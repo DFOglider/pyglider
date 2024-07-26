21da4a0a402fefcaa9768376ad27928d6dd88fc1
Layton <layton.chantelle@gmail.com>
2024-07-26 11:07:24 -0300

    revert interpolate check to original code
    when outputting yaml files using yaml.dump, boolean values are lowercased. But python is smart to know that it is boolean when the yaml is read.

--- pyglider/seaexplorer.py
+++ pyglider/seaexplorer.py
@@ -369,14 +369,8 @@ def raw_to_timeseries(indir, outdir, deploymentyaml, kind='raw',
     thenames = list(ncvar.keys())
     # Check yaml to see if interpolate has been set to True
     if "interpolate" in thenames:
-        # check for common non-boolean values that could be in deploymentyaml
-        if not(isinstance(ncvar["interpolate"], bool)):
-            if ncvar["interpolate"] in ('false', 0, 'no'):
-                interpolate = False
-            if ncvar["interpolate"] in ('true', 1, 'yes'):
-                interpolate = True
-        else:
-            interpolate = ncvar["interpolate"]
+        if ncvar["interpolate"]:
+            interpolate = True
     for i in ['time', 'timebase', 'keep_variables', 'interpolate']:
         if i in thenames:
             thenames.remove(i)
